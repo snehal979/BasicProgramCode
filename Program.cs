ef58eaf9e2a62a2221f3ac3edfd3a547f723928a
Snehal979 <bansodsnehal535@gmail.com>
2023-08-16 21:17:29 +0530

    Display Five Name My Frd List

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Basic Program Code");
+            Program program = new Program();
+            program.DisplayFriendName();
+        }
+        //Display Frd Name 
+        public void DisplayFriendName()
+        {
+            string[] frdArray = { "Snehal", "Madhavi", "Mayur", "Anu", "Madhav" };
+            Console.WriteLine("Display My Friend List");
+            foreach (string frd in frdArray)
+            {
+                Console.WriteLine(frd);
+            }
         }
     }
 }
