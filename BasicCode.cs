f0816f2cd4bbe28985baea9cf1405213324bdd18
Snehal979 <bansodsnehal535@gmail.com>
2023-08-16 23:12:11 +0530

    Calculate the circle area and Perimeter

--- BasicCode.cs
+++ BasicCode.cs
@@ -29,6 +29,26 @@ namespace BasicProgramCode
 
              avg = (num1+ num2+num3+ num4+num5)/5;
             Console.WriteLine("Average of Five random Number is :"+avg);
+           
+        }
+        //Compute perimeter and area of a circle.
+        //Take diameter of circle as user input,
+        //Compute and print out perimeter and area of circle.
+        public void CircleCalculation()
+        {
+            const double PI_VALUE = 3.14;
+            //C=2πr perimeter
+            //A=πr*r Area
+
+            Console.WriteLine("Enter the diameter of circle");
+            double diameter = Convert.ToDouble(Console.ReadLine());
+            double area, perimeter;
+            perimeter = PI_VALUE* diameter;
+
+            double radius = diameter/2;
+            area = PI_VALUE*Math.Pow(radius,2);
+            Console.WriteLine("Perimeter of circle is {0} and area of circle is {1}",perimeter, area) ;
+
         }
     }
 }
