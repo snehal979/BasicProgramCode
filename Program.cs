e541f09cdf6f32053786ea135db06bcf2c259b55
Snehal979 <bansodsnehal535@gmail.com>
2023-08-16 21:06:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicProgramCode
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Program Code");
+        }
+    }
+}
